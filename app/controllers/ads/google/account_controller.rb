70197b9d1a4fb6c2f50840878fe90ca219ee7fad
Tranxuanvu <xuanvu442@gmail.com>
2016-04-25 10:27:33 +0700

    add missing function

--- app/controllers/ads/google/account_controller.rb
+++ app/controllers/ads/google/account_controller.rb
@@ -3,7 +3,6 @@ class Ads::Google::AccountController < Ads::Google::MasterController
     @selected_account = selected_account
     @accounts = selected_account ? [] : get_all_client_accounts
     @current_account = Account.get_current_account(get_account_list())
-
   end
 
   def select()
@@ -24,8 +23,13 @@ class Ads::Google::AccountController < Ads::Google::MasterController
   end
 
   def client_accounts
+    result = []
     graph = get_accounts_graph_with_fields(['CustomerId'])
-    graph[:links].map{ |link| link[:client_customer_id] }
+    if graph && graph[:links] #&& graph[:entries]
+      result = graph[:links].map{ |link| link[:client_customer_id] }
+      # result = graph[:entries].select{ |e| ids.include?(e[:customer_id]) }
+    end
+    result
   end
 
   def get_client_customer_id
@@ -96,5 +100,31 @@ class Ads::Google::AccountController < Ads::Google::MasterController
     als = get_account_label(customer_id)
     account.account_labels = als
     account
+  end
+
+  def get_accounts_graph_with_fields(fields)
+    result = nil
+    begin
+      # First get the AdWords manager account ID.
+      customer_srv = adwords.service(:CustomerService, get_api_version())
+      customer = customer_srv.get()
+      adwords.credential_handler.set_credential(
+          :client_customer_id, customer[:customer_id])
+
+      # Then find all child accounts using that ID.
+      managed_customer_srv = adwords.service(
+          :ManagedCustomerService, get_api_version())
+      selector = {:fields => fields}
+
+      result = managed_customer_srv.get(selector)
+    rescue AdwordsApi::Errors::ApiException => e
+      logger.fatal("Exception occurred: %s\n%s" % [e.to_s, e.message])
+      flash.now[:alert] =
+          'API request failed with an error, see logs for details'
+    rescue NoMethodError => e
+      [:selected_account, :token].each {|key| session.delete(key)}
+      redirect_to ads_google_login_prompt_path, notice: 'Your google authentication is out of date. Login to continue.' and return nil
+    end
+    return result
   end
 end
